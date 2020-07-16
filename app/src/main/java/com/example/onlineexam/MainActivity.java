34911f51c05d0040e11aa75e1f00b2dcb135cf7b
Ying-Chau Huang <yingchauhuang@gmail.com>
2020-07-16 11:24:09 +0800

    second commit

--- app/src/main/java/com/example/onlineexam/MainActivity.java
+++ app/src/main/java/com/example/onlineexam/MainActivity.java
@@ -7,7 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+// This is Vivian's stuff
 public class MainActivity extends AppCompatActivity {
 
     @Override
